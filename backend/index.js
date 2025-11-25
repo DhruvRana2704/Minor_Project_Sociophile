6f682aeca377862a86cd797791c5f7a319facbe6
Dhruv Rana <ddhruvrana@gmail.com>
2025-11-25 14:05:23 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -60,4 +60,4 @@ app.use('/followers', require('./routes/follower'));
 app.use('/likes', require('./routes/like'));
 app.use('/interactions', require('./routes/interaction'));
 
-app.listen(port,()=>console.log(`Server running on port ${port}`));
+app.listen(port,'0.0.0.0',()=>console.log(`Server running on port ${port}`));
